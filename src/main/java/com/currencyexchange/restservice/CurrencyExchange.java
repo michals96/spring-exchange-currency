31158dbe3f8b0f2eeeb2c03799ee957f75a4a521
michals96 <michal.stefaniuk9@gmail.com>
2020-09-23 21:35:05 +0200

    Fixed exchangeCurrency controller

--- src/main/java/com/currencyexchange/restservice/CurrencyExchange.java
+++ src/main/java/com/currencyexchange/restservice/CurrencyExchange.java
@@ -1,19 +1,35 @@
 package com.currencyexchange.restservice;
 
-import javax.money.CurrencyUnit;
+import javax.money.NumberValue;
+import javax.money.convert.ExchangeRate;
+import javax.money.convert.ExchangeRateProvider;
+import javax.money.convert.MonetaryConversions;
 
 public class CurrencyExchange {
 
     private final long id;
-    private final Integer amount;
+    private final Double amount;
     private final String firstCurrency;
     private final String secondCurrency;
 
+    public Double convertedAmount;
+
     public CurrencyExchange(long id, String firstCurrency, String secondCurrency) {
         this.id = id;
-        this.amount = 100;
+        this.amount = 100.0;
         this.firstCurrency = firstCurrency;
         this.secondCurrency = secondCurrency;
+        this.convertedAmount = 0.0;
+    }
+
+    public void converter()
+    {
+        ExchangeRateProvider exchangeRateProvider = MonetaryConversions.getExchangeRateProvider();
+        ExchangeRate rate = exchangeRateProvider.getExchangeRate(getFirstCurrency(), getSecondCurrency());
+
+        NumberValue factor = rate.getFactor();
+
+        convertedAmount = Double.parseDouble(factor.toString()) * amount;
     }
 
     public long getId() {
@@ -28,7 +44,15 @@ public class CurrencyExchange {
         return secondCurrency;
     }
 
-    public Integer getAmount(){
+    public Double getAmount(){
         return amount;
+    }
+
+    public Double getConvertedAmount(){
+        return convertedAmount;
+    }
+
+    public String responseString() {
+        return getId() + ": " + getAmount() + " " + getFirstCurrency() + " = " + getConvertedAmount() + " " + getSecondCurrency();
     }
 }
--- src/main/java/com/currencyexchange/restservice/CurrencyExchangeController.java
+++ src/main/java/com/currencyexchange/restservice/CurrencyExchangeController.java
@@ -2,19 +2,20 @@ package com.currencyexchange.restservice;
 
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class CurrencyExchangeController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/currencyExchange", method = RequestMethod.GET)
-    public CurrencyExchange currencyExchange( @RequestParam Map<String, String> requestParams) throws Exception {
+    public String currencyExchange( @RequestParam Map<String, String> requestParams) throws Exception {
 
-        return new CurrencyExchange(counter.incrementAndGet(), requestParams.get("firstCurrency"), requestParams.get("secondCurrency"));
+        CurrencyExchange currencyExchange = new CurrencyExchange(counter.incrementAndGet(), requestParams.get("firstCurrency"), requestParams.get("secondCurrency"));
+
+        currencyExchange.converter();
+
+        return currencyExchange.responseString();
     }
 }
 
