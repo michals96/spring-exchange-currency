0b19ab93ea527cc41679f437934c26aff8dbdfe7
michals96 <michal.stefaniuk9@gmail.com>
2020-11-22 14:22:08 +0100

    Comment out exchange with database interaction

--- src/main/java/com/currencyexchange/controllers/CurrencyExchangeController.java
+++ src/main/java/com/currencyexchange/controllers/CurrencyExchangeController.java
@@ -37,8 +37,13 @@ public class CurrencyExchangeController {
         return ratesRepository.findByCurrenciesAndDate(sourceCurrency, targetCurrency, date).get(0).getRate();
     }
 
-    @GetMapping(value = "/currencyExchange/{sourceCurrency}/{targetCurrency}/{amount}")
-    public CurrencyExchange currencyExchange( @PathVariable String sourceCurrency, @PathVariable String targetCurrency, @PathVariable Double amount) throws Exception {
+    @GetMapping(value = "/currencyExchangeNoApi/{sourceCurrency}/{targetCurrency}/{amount}")
+    public CurrencyExchange currencyExchangeNoApi( @PathVariable String sourceCurrency, @PathVariable String targetCurrency, @PathVariable Double amount) throws Exception {
+        return currencyExchangeService.convert(sourceCurrency, targetCurrency, amount);
+    }
+
+    @GetMapping(value = "/currencyExchangeApi/{sourceCurrency}/{targetCurrency}/{amount}")
+    public CurrencyExchange currencyExchangeApi( @PathVariable String sourceCurrency, @PathVariable String targetCurrency, @PathVariable Double amount) throws Exception {
         return currencyExchangeService.convert(sourceCurrency, targetCurrency, amount);
     }
 
@@ -49,29 +54,28 @@ public class CurrencyExchangeController {
         return currencyExchangeService.convertWithAllApi(sourceCurrency, targetCurrency, amount);
     }
 
-    @GetMapping(value = "/currencyExchangeApi/{serviceType}/{sourceCurrency}/{targetCurrency}/{amount}")
-    public CurrencyExchange currencyExchangeApi(@PathVariable String serviceType,
+    @GetMapping(value = "/currencyExchangeSelectedApi/{serviceType}/{sourceCurrency}/{targetCurrency}/{amount}")
+    public CurrencyExchange currencyExchangeSelectedApi(@PathVariable String serviceType,
                                                 @PathVariable String sourceCurrency,
                                                              @PathVariable String targetCurrency,
                                                              @PathVariable Double amount) throws Exception {
         // ten if else to repozytorium
-        if(ratesExistsInDatabase(sourceCurrency, targetCurrency, LocalDate.now())){
+       /* if(ratesExistsInDatabase(sourceCurrency, targetCurrency, LocalDate.now())){
 
             Double rate = fetchRate(sourceCurrency, targetCurrency, LocalDate.now());
             Double convertedAmount = amount * rate;
             return new CurrencyExchange(0, sourceCurrency, targetCurrency, 100.0, convertedAmount, rate, LocalDate.now());
         }
-        else{
+        else{*/
 
             CurrencyExchange currencyExchange = currencyExchangeService.convertWithApi(sourceCurrency, targetCurrency, amount, Class.forName("com.currencyexchange.repositories." + serviceType));
-            addRatesToDatabase(currencyExchange);
+            //addRatesToDatabase(currencyExchange);
             return currencyExchange;
-        }
-        // tutaj jeden return zamiast dwoch w instrukcji if else
+        //}
     }
 }
 
-// Example invocation: http://localhost:8080/currencyExchangeApi/JavaCurrencyExchangeRepository/USD/GBP/100.0
+// Example invocation: http://localhost:8080/currencyExchangeSelectedApi/JavaCurrencyExchangeRepository/USD/GBP/100.0
 // DB Access: http://localhost:8080/h2-console
 
 // Tabela do currencies(statycznie zawiera wszystkie waluty które ja zezwalam, jesli nie ma to exceptiem) -> w tabeli Rates przetrzymywać ID currency
